ae40774871c5ee457bcb6baf6a7b0d79ad67e606
nkoron@itba.edu.ar <nkoron@itba.edu.ar>
2024-08-21 15:06:25 -0300

    comments

---
+++ slave.c
@@ -0,0 +1,16 @@
+#include <sys/types.h>
+#include <sys/wait.h>
+
+
+int main (int argc, const char * argv[]){
+    char path[100] ;
+// hacer read, tener en cuenta que puede venir mas de un file a la vez
+// hacer chequeo de que se leyo algo
+// manejar buffer de lectura, no agarrar de a un caracter
+
+
+
+// llamar a md5 (investigar el uso de system || o popen() )
+// enviar nombre y md5 al padre (ver como)
+
+}
---
+++ view.c
@@ -0,0 +1,12 @@
+#include <sys/types.h>
+#include <sys/wait.h>
+
+
+int main (int argc, const char * argv[]) {
+// primero ver si se recibio argumentos (o sea el buffer)
+// dps hacer lectura a ver si recibio por pipe (si no recibio por args)
+// imprimir para cada elemento del buffer los datos (md5 PID fileName)
+// queda ver como hacer para que deje de esperar inputs en el buffer compartido (ver video) (semforos)
+// preguntar si se crea desde aca el shared memory
+//
+}
