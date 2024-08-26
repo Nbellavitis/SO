cc19970cfb674766438099e5994f009ab37fce83
Nbellavitis <nbellavitisalzate@itba.edu.ar>
2024-08-26 14:39:49 -0300

    cositas

--- md5.c
+++ md5.c
@@ -4,16 +4,22 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
-#define CHILD_QTY 10
+#define MAX_CHILD_QTY 10
 #define INITIAL_FILES_PER_CHILD 2
-int parent_to_child_pipe[CHILD_QTY][2];
-int child_to_parent_pipe[CHILD_QTY][2];
-int  child_pid[CHILD_QTY];
+#define MAX_MD5 32
+#define MAX_PATH 128
+#define MIN(x,y) (x<y) ? x:y 
+char path[100];
 int pipe_read(int fd, char *buff);
 FILE * resultado;
 int main(int argc, const char *argv[]){
- int files_assigned=1;
-    for(int i=0;i<CHILD_QTY;i++){
+    int child_qty=MIN(MAX_CHILD_QTY,argc-1);
+    int parent_to_child_pipe[child_qty][2];
+    int child_to_parent_pipe[child_qty][2];
+    int  child_pid[child_qty];
+    resultado= fopen("salida.txt", "wr");
+    int files_assigned=1;
+    for(int i=0;i<child_qty;i++){
         pipe(parent_to_child_pipe[i]);
         pipe(child_to_parent_pipe[i]);
         if ( (child_pid[i]=fork())== 0) {   // soy hijo
@@ -35,15 +41,31 @@ int main(int argc, const char *argv[]){
 
  int total_files_to_process=argc-1;
 for(int i=0;i<total_files_to_process;i++){
-    for(int child_index=0;child_index<CHILD_QTY && files_assigned <= total_files_to_process;child_index++,files_assigned++){
+    for(int child_index=0;child_index<child_qty && files_assigned <= total_files_to_process;child_index++,files_assigned++){
         write(parent_to_child_pipe[child_index][1],argv[files_assigned],strlen(argv[files_assigned])+1);  
     }
 }
+   fd_set readfds;
+    int max_fd = -1;
 
+for (int i = 0; i < argc-1; i++) {
+    FD_SET(child_to_parent_pipe[i%child_qty][0], &readfds);
+            if (FD_ISSET(child_to_parent_pipe[i%child_qty][0], &readfds)) {
+                int bytes_read = pipe_read(child_to_parent_pipe[i%child_qty][0], path);
+                if (bytes_read < 0) {
+                    perror("read");
+                    exit(EXIT_FAILURE);
+                } else if (bytes_read == 0) {
+                    close(child_to_parent_pipe[i%child_qty][0]);
+                } else {
+                    fprintf(resultado, "ID:%d MD5:%s\n", child_pid[i%child_qty], path);
+                    fflush(resultado);
+                }
+            }}
 exit(EXIT_SUCCESS);
 
-}
 
+}
 int pipe_read(int fd, char *buff){
     int i=0;
     char last_charater_read[1];
--- slave.c
+++ slave.c
@@ -3,17 +3,33 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#define MAX_MD5 32
+#define MAX_PATH 128
+#define OFFSET 3
 int pipe_read(int fd, char *buff);
 int main (int argc, const char * argv[]){
 
-    char path[100] ;
+    char path[MAX_PATH] ;
+    char *md5_cmd="md5sum %s";
+    
+    char command[MAX_PATH + strlen(md5_cmd)];
     while(pipe_read(STDIN_FILENO, path)>0){
           if (path[0] == 0) {
             // No more files to read, exit the loop
             break;
         }
-        write(STDOUT_FILENO,"hola",strlen("hola")+1);
+        char md5[MAX_MD5+strlen(path)+OFFSET];
+        sprintf(command,md5_cmd,path);
+        FILE * fp=popen(command,"r");
+             if (fp == NULL) {
+            perror("popen");
+            exit(EXIT_FAILURE);
+        }
+        fgets(md5, MAX_MD5+strlen(path), fp);
+        pclose(fp);
+        write(STDOUT_FILENO,md5,strlen(md5)+1);
     }
+    close(STDOUT_FILENO);
     exit(EXIT_SUCCESS);
  
  
