330a8a755640544a436c7b2ef94fef30ab0ab4e2
Nbellavitis <nbellavitisalzate@itba.edu.ar>
2024-08-28 16:30:01 -0300

    working :D

--- md5.c
+++ md5.c
@@ -8,9 +8,10 @@
 #define INITIAL_FILES_PER_CHILD 2
 #define MAX_MD5 32
 #define MAX_PATH 128
-#define MIN(x,y) (x<y) ? x:y 
+#define MIN(x,y) (x<y) ? x:y
+#define MAX(x,y) (x>y) ? x:y 
 char path[100];
-
+void close_pipes_that_are_not_mine(int parent_to_child_pipe[][2], int child_to_parent_pipe[][2], int index);
 void set_fd(int child_to_parent_pipe[][2],int flag, int * max_fd, fd_set * readfds,int child_qty);
 FILE * resultado;
 
@@ -18,6 +19,9 @@ int is_fd_open(int fd) {
     if (fcntl(fd, F_GETFD) == -1) {
         if (errno == EBADF) {
             return 0;  // File descriptor is not open
+        }else{
+            perror("closed");
+            exit(EXIT_FAILURE);
         }
     }
     return 1;  // File descriptor is open
@@ -63,7 +67,6 @@ void create_child_process(int parent_to_child_pipe[][2], int child_to_parent_pip
         exit(EXIT_FAILURE);
     }else if( child_pid[index]== 0) {
 
-        close_pipes_that_are_not_mine(parent_to_child_pipe,child_to_parent_pipe,index);
         handle_pipes_child(parent_to_child_pipe, child_to_parent_pipe, index);
 
 
@@ -79,24 +82,15 @@ void create_pipes(int parent_to_child_pipe[][2], int child_to_parent_pipe[][2], 
     }
 }
 
-void send_to_process(int child_index, int quantity, int * files_assigned, int parent_to_child_pipe[][2], const char *argv[]){
-    for(int i=0;i<quantity;i++) {
+void send_to_process(int child_index, int quantity, int * files_assigned,int total_files_to_process, int parent_to_child_pipe[][2], const char *argv[]){
+    for(int i=0;i<quantity && *files_assigned <= total_files_to_process;i++) {
         write(parent_to_child_pipe[child_index][1], argv[*files_assigned], strlen(argv[*files_assigned]) + 1); // revisar argv
         (*files_assigned)++;
     }
 }
 void send_initial_files(int child_qty, int total_files_to_process, int * files_assigned , int parent_to_child_pipe[][2], const char *argv[]){
     for(int child_index=0; child_index<child_qty && *files_assigned <= total_files_to_process;child_index++){
-        send_to_process(child_index, INITIAL_FILES_PER_CHILD, files_assigned,parent_to_child_pipe, argv);
-    }
-}
-void close_pipes_that_are_not_mine(int parent_to_child_pipe[][2], int child_to_parent_pipe[][2], int index){
-    for(int i=0; i< index; i++){
-        for(int j=0; j<2; j++){
-            close(parent_to_child_pipe[i][j]);
-            close(child_to_parent_pipe[i][j]);
-        }
-
+        send_to_process(child_index, MIN(total_files_to_process,INITIAL_FILES_PER_CHILD), files_assigned,total_files_to_process,parent_to_child_pipe, argv);
     }
 }
 
@@ -124,57 +118,31 @@ int main(int argc, const char *argv[]){
     int max_write=-1;
     int file_index=0;
     while(file_index < total_files_to_process){ // hay que corregir esto --creo que ahi esta? (feo igual )
+        FD_ZERO(&readfds);FD_ZERO(&writefds);
         set_fd(child_to_parent_pipe,0,&max_read,&readfds,child_qty);
         set_fd(parent_to_child_pipe,1,&max_write,&writefds,child_qty);
-        //select(MAX(max_read,max_write)+1,&readfds,&writefds,NULL,NULL); (ESTO NO FUNCIONA NO SE PORQUE AAAA)
-        select(max_read+1,&readfds,NULL,NULL,NULL);
-        select(max_write+1,NULL,&writefds,NULL,NULL);
+         select(max_read+1,&readfds,NULL,NULL,NULL);
+         select(max_write+1,NULL,&writefds,NULL,NULL);
         for(int i=0;i<child_qty;i++){
             if (FD_ISSET(child_to_parent_pipe[i][0], &readfds)) {
                 int bytes_read = pipe_read(child_to_parent_pipe[i][0], path);
                 if (bytes_read < 0) {
                     perror("read");
                     exit(EXIT_FAILURE);
-                } else if (bytes_read == 0) {
-                    close(child_to_parent_pipe[i][0]);
                 } else {
                     fprintf(resultado, "ID:%d MD5:%s\n", child_pid[i], path);
                     fflush(resultado);
                     if (files_assigned < argc && FD_ISSET(parent_to_child_pipe[i][1], &writefds)) {
-                        send_to_process(i, 1, &files_assigned, parent_to_child_pipe, argv);
+                        send_to_process(i, 1, &files_assigned,total_files_to_process, parent_to_child_pipe, argv);
                     }
                     file_index++;
                 }
             }
         }
     }
-//close_pipes(child_to_parent_pipe, parent_to_child_pipe, child_qty);
+close_pipes(child_to_parent_pipe, parent_to_child_pipe, child_qty);
 
-    for (size_t i = 0; i < child_qty; i++)
-    {
-        if(is_fd_open(child_to_parent_pipe[i][0])){
-            fprintf(resultado,"Child to parent, read %d\n",i);
-            fflush(resultado);
-            close(child_to_parent_pipe[i][0]);
-        }
-        if(is_fd_open(child_to_parent_pipe[i][1])){
-            fprintf(resultado,"Child to parent, write %d\n",i);
-            fflush(resultado);
-            close(child_to_parent_pipe[i][1]);
-        }
-        if(is_fd_open(parent_to_child_pipe[i][0])){
-            fprintf(resultado,"parent to child, read %d\n",i);
-            fflush(resultado);
-            close(parent_to_child_pipe[i][0]);
-        }
-        if(is_fd_open(parent_to_child_pipe[i][1])){
-            fprintf(resultado,"parent to child, write %d\n",i);
-            fflush(resultado);
-            close(parent_to_child_pipe[i][1]);
-
-        }
-    }
-
+fclose(resultado);
 exit(EXIT_SUCCESS);
 }
 
