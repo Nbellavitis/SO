f234377ef264b79201906573679a46dba09514dc
ivovila <ivilamowski@itba.edu.ar>
2024-09-11 19:37:24 -0300

    Read error

--- pipe_master.c
+++ pipe_master.c
@@ -3,19 +3,14 @@
 #include "commons.h"
 #include "pipe_master.h"
 
-
-int pipe_read(int fd, char *buff){
-    int i=0;
-    char last_charater_read[1];
-    last_charater_read[0]=1;
-
-    while(last_charater_read[0]!=0 && last_charater_read[0]!='\n' && read(fd,last_charater_read,1)>0){
-        buff[i++]=last_charater_read[0];
-    }
-    buff[i]=0;
-
-    return i;
+#define MAX_PATH 128
+int pipe_read(int fd, char *buff) {
+    int bytes_read = read(fd, buff, MAX_PATH);
+    buff[bytes_read-1] = '\0';
+    return bytes_read;
 }
+
+
 
 void handle_pipes_child(int parent_to_child_pipe[][2], int child_to_parent_pipe[][2], int index){
     close(parent_to_child_pipe[index][1]);
